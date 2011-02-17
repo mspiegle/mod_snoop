8d58fd17a432c9b47a23e1f757a7680d4e69b731
Michael Spiegle <mike@nauticaltech.com>
2011-02-17 01:20:50 -0800

    Skeleton source/header files

---
+++ include/mod_snoop.h
@@ -0,0 +1,10 @@
+/*
+ * mod_snoop.h - A support header for mod_snoop
+ * Copyright (C) 2011 Michael Spiegle (mike@nauticaltech.com)
+ *
+ */
+
+#ifndef _MOD_SNOOP_H_
+#define _MOD_SNOOP_H_
+
+#endif
---
+++ src/mod_snoop.c
@@ -0,0 +1,7 @@
+/* 
+ * mod_snoop.c - The main implementation of mod_snoop
+ * Copyright (C) 2011 Michael Spiegle (mike@nauticaltech.com)
+ *
+ */
+
+#include "mod_snoop.h"
