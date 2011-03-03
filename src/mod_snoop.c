86ba4d48fe72386a8cb1b671fae0f7ac672dc8af
Michael Spiegle <mike@nauticaltech.com>
2011-03-02 20:15:54 -0800

    Finally getting this thing to where it should be.  I just snooped my first request.

--- src/mod_snoop.c
+++ src/mod_snoop.c
@@ -24,59 +24,118 @@ capture_filter(ap_filter_t* f, apr_bucket_brigade* bb, ap_input_mode_t mode,
                apr_read_type_e block, apr_off_t readbytes) {
 
 	printf("Inside capture_filter(%d, %d, %d)\n", mode, block, readbytes);
-	// we're only concerned with AP_MODE_GETLINE
+
+	//we're only concerned with AP_MODE_GETLINE
 	if (mode != AP_MODE_GETLINE) {
 		return ap_get_brigade(f->next, bb, mode, block, readbytes);
 	}
 
-	// go ahead and fetch the brigade - deal with errors
+	//setup the filter ctx
 	apr_status_t ret;
+	snoop_filter_ctx_t* ctx = f->ctx;
+	if (ctx == NULL) {
+		// allocate ctx
+		printf("Allocating a ctx...\n");
+		f->ctx = ctx = apr_pcalloc(f->c->pool, sizeof(snoop_filter_ctx_t));
+
+		//create a subpool for the bb
+		if (APR_SUCCESS != (ret = apr_pool_create(&(ctx->pool), f->c->pool))) {
+			ap_log_error(APLOG_MARK, APLOG_WARNING, 0, f->c->base_server,
+			             "capture_filter(): couldn't create pool");
+		}
+
+		//create a bucket brigade
+		ctx->bb = apr_brigade_create(ctx->pool, f->c->bucket_alloc);
+
+		//set start
+		ctx->state = SNOOP_REQUEST_START;
+	}
+
+	//if we already processed the request, then move on
+	if (ctx->state == SNOOP_REQUEST_END) {
+		return ap_get_brigade(f->next, bb, mode, block, readbytes);
+	}
+
+	//we have reason to believe that we should snoop this request.  do it.
 	if (APR_SUCCESS != (ret = ap_get_brigade(f->next, bb, mode,
 	                                         block, readbytes))) {
 		return ret;
 	}
 
-	// setup the filter ctx
-	snoop_filter_ctx_t* ctx;
-	if (NULL == f->ctx) {
-		// allocate ctx
-		f->ctx = ctx = apr_pcalloc(f->f->pool, sizeof(snoop_filter_ctx_t));
-
-		// create a subpool for the bb
-		apr_status_t ret;
-		if (APR_SUCCESS != (ret = apr_pool_create(&(ctx->pool), f->r->pool))) {
-			ap_log_error(APLOG_MARK, APLOG_WARNING, 0, f->r->server,
-			             "capture_filter(): couldn't create pool");
-		}
-
-		// create a bucket brigade
-		ctx->bb = apr_brigade_create(ctx->pool, f->c->bucket_alloc);
-
-		// set start
-		ctx->state = SNOOP_REQUEST_START;
-	}
-
-	// if we got this far, then we have bb data and a valid ctx
-	const char* buf;
+	//if we got this far, then we have bb data and a valid ctx
+	const char* buffer;
 	apr_size_t br;
 	apr_bucket* b;
 	b = APR_BRIGADE_FIRST(bb);
-	printf("Starting loop\n");
+	//iterate our incoming bucket brigade
 	while (b != APR_BRIGADE_SENTINEL(bb)) {
-		printf("Doing another iteration\n");
-		// show us the DATA
 		if (!APR_BUCKET_IS_METADATA(b)) {
-			if (APR_SUCCESS == apr_bucket_read(b, &buf, &br, APR_BLOCK_READ)) {
-				//printf("%s\n", buf);
-				// we should parse through the bucket until we find \r\n\r\n
-				int x = 0;
-				for (x = 0; x < br; x++) {
-					
+			if (APR_SUCCESS == apr_bucket_read(b, &buffer, &br, APR_BLOCK_READ)) {
+				//check to see if this is the end of the request
+				if (br == 2) {
+					if (0 == strncmp(buffer, "\r\n", 2)) {
+						//cleanup and ensure we don't run anymore
+						if (APR_SUCCESS != (ret = apr_brigade_pflatten(ctx->bb, &buffer,
+						                                               &br, f->c->pool))) {
+							ap_log_error(APLOG_MARK, APLOG_CRIT, 0, f->c->base_server,
+							             "capture_filter(): couldn't pflatten brigade");
+							return ret;
+						}
+
+						//get socket
+						apr_socket_t* socket;
+						if (APR_SUCCESS != (ret = apr_socket_create(&socket, APR_INET,
+						                                            SOCK_DGRAM,
+						                                            APR_PROTO_UDP,
+						                                            f->c->pool))) {
+							ap_log_error(APLOG_MARK, APLOG_CRIT, 0, f->c->base_server,
+							             "capture_filter(): couldn't allocate socket");
+							return ret;
+						}
+
+						//get target
+						apr_sockaddr_t* target;
+						if (APR_SUCCESS != (ret = apr_sockaddr_info_get(&target,
+						                                                "localhost",
+						                                                APR_UNSPEC, 9876,
+						                                                0, f->c->pool))) {
+							ap_log_error(APLOG_MARK, APLOG_CRIT, 0, f->c->base_server,
+							             "capture_filter(): couldn't lookup target");
+							return ret;
+						}
+
+						//send packet
+						if (APR_SUCCESS != (ret = apr_socket_sendto(socket, target, 0,
+						                                            buffer, &br))) {
+							ap_log_error(APLOG_MARK, APLOG_CRIT, 0, f->c->base_server,
+							             "capture_filter(): couldn't send packet");
+							return ret;
+						}
+
+						ctx->state = SNOOP_REQUEST_END;
+						return APR_SUCCESS;
+					}
 				}
+
+				//otherwise, keep appending input buckets to our temp bb
+				apr_bucket* temp = NULL;
+				if (APR_SUCCESS != (ret = apr_bucket_copy(b, &temp))) {
+					ap_log_error(APLOG_MARK, APLOG_CRIT, 0, f->c->base_server,
+					             "capture_filter(): failed to copy bucket");
+					return ret;
+				}
+
+				if (APR_SUCCESS != (ret = apr_bucket_setaside(temp, f->c->pool))) {
+					ap_log_error(APLOG_MARK, APLOG_CRIT, 0, f->c->base_server,
+					             "capture_filter(): failed to setaside bucket");
+					return ret;
+				}
+
+				APR_BRIGADE_INSERT_TAIL(ctx->bb, temp);
 			}
 		}
 
-		// load next bucket
+		//load next bucket
 		b = APR_BUCKET_NEXT(b);
 	}
 
@@ -85,9 +144,7 @@ capture_filter(ap_filter_t* f, apr_bucket_brigade* bb, ap_input_mode_t mode,
 
 static int
 pre_connection(conn_rec* c, void* csd) {
-	//ap_add_input_filter(capture_filter_name, NULL, NULL, c);
 	ap_add_input_filter("SNOOP", NULL, NULL, c);
-	printf("*** pre_connection ***\n");
 	return OK;
 }
 
@@ -101,7 +158,6 @@ register_hooks(apr_pool_t* pool) {
 	ap_register_input_filter("SNOOP", capture_filter, NULL,
 	                         AP_FTYPE_CONNECTION);
 	ap_hook_pre_connection(pre_connection, NULL, NULL, APR_HOOK_MIDDLE);
-	printf("*** register_hooks ***\n");
 }
 
 static const
